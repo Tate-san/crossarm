8723ec42e53b4d3c962323b6c294e9534edb1ef9
Matyas Burnek <burnek.matyas@gmail.com>
2024-06-12 15:34:52 +0200

    feat: moved to older toolchain for wider GLIBC compatibility

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ function print_help {
 function set_arch {
 	case "$1" in
 		"arm" )
-			TOOLCHAIN_URL="https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-linux-gnueabihf.tar.xz"
+			TOOLCHAIN_URL="https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.3-2021.07/binrel/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf.tar.xz"
 			;;
 		"aarch64" )  
 			TOOLCHAIN_URL="https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.3-2021.07/binrel/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu.tar.xz"
